c491c0460f1f97c15bb38f05a38ce35cb033402c
Braydon Fuller <courier@braydon.com>
2016-07-07 15:03:47 -0400

    Add regtest for balance updates

--- regtest/index.js
+++ regtest/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var async = require('async');
 var chai = require('chai');
 var bitcore = require('bitcore-lib');
 var BitcoinRPC = require('bitcoind-rpc');
@@ -13,6 +14,7 @@ var Config = index.Config;
 
 var testWIF = 'cSdkPxkAjA4HDr5VHgsebAPDEh9Gyub4HK8UJr2DFGGqKKy4K5sG';
 var testKey = bitcore.PrivateKey(testWIF);
+var testAddress = testKey.toAddress('regtest').toString();
 var bitcoinClient;
 var server;
 var client;
@@ -21,15 +23,38 @@ describe('Wallet Server & Client', function() {
 
   var regtest;
 
+  function sendAndGenerate(address, amount, done) {
+    var data = {};
+    bitcoinClient.sendToAddress(testAddress, amount, function(err, response) {
+      if (err) {
+        return done(err);
+      }
+      data.txid = response.result;
+      bitcoinClient.generate(1, function(err, response) {
+        if (err) {
+          return done(err);
+        }
+        data.blockHash = response.result[0];
+        done(null, data);
+      });
+    });
+  }
+
   before(function(done) {
     this.timeout(10000);
 
     var configPath = __dirname + '/data';
 
-    rimraf(configPath + '/bitcoin/regtest', function(err) {
-
+    async.series([
+      function(next) {
+        rimraf(configPath + '/bitcoin/regtest', next);
+      },
+      function(next) {
+        rimraf(configPath + '/regtest.lmdb', next);
+      }
+    ], function(err) {
       if (err) {
-        throw err;
+        return done(err);
       }
 
       server = new Server({network: 'regtest', configPath: configPath});
@@ -60,9 +85,14 @@ describe('Wallet Server & Client', function() {
         });
 
         var syncedHandler = function(height) {
+          // check that the block chain is generated
           if (height >= 150) {
             server.node.services.bitcoind.removeListener('synced', syncedHandler);
-            done();
+
+            // check that client can connect
+            async.retry({times: 5, interval: 2000}, function(next) {
+              client.getInfo(next);
+            }, done);
           }
         };
 
@@ -96,13 +126,55 @@ describe('Wallet Server & Client', function() {
     });
   });
   it('will import an address', function(done) {
-    var address = testKey.toAddress().toString();
-    client.importAddress(walletId, address, function(err, result) {
+    client.importAddress(walletId, testAddress, function(err, result) {
       if (err) {
         return done(err);
       }
       should.exist(result);
       done();
+    });
+  });
+  describe('wallet block updates', function() {
+    var expected;
+    before(function(done) {
+      this.timeout(10000);
+      sendAndGenerate(testAddress, 10, function(err, response) {
+        if (err) {
+          return done(err);
+        }
+        expected = response;
+        // TODO wait until height is updated
+        setTimeout(done, 1000);
+      });
+    });
+    it('will update the balance for the wallet', function(done) {
+      client.getBalance(walletId, function(err, result) {
+        if (err) {
+          return done(err);
+        }
+        result.balance.should.equal(10 * 1e8);
+        done();
+      });
+    });
+    it('will get the latest txids', function(done) {
+      client.getTxids(walletId, {}, function(err, result) {
+        if (err) {
+          return done(err);
+        }
+        result.txids.length.should.equal(1);
+        result.txids[0].should.equal(expected.txid);
+        done();
+      });
+    });
+    it('will get the latest transactions', function(done) {
+      client.getTransactions(walletId, {}, function(err, result) {
+        if (err) {
+          return done(err);
+        }
+        result.transactions.length.should.equal(1);
+        result.transactions[0].txid.should.equal(expected.txid);
+        done();
+      });
     });
   });
 
