76b075ceb4a9c46e0c50773bd159375130229665
Braydon Fuller <courier@braydon.com>
2016-07-27 17:25:20 -0400

    Unit tests for utilities

--- test/utils.unit.js
+++ test/utils.unit.js
@@ -4,10 +4,40 @@ var chai = require('chai');
 var should = chai.should();
 var sinon = require('sinon');
 var proxyquire = require('proxyquire');
+var EventEmitter = require('events').EventEmitter;
 
 var utils = require('../lib/utils');
 
 describe('Wallet Utils', function() {
+  describe('#isInteger', function() {
+    it('will return false for 0.1', function() {
+      utils.isInteger(0.1).should.equal(false);
+    });
+    it('will return false for "0.1"', function() {
+      utils.isInteger("0.1").should.equal(false);
+    });
+    it('will return false for NaN', function() {
+      utils.isInteger(NaN).should.equal(false);
+    });
+    it('will return false for Infinity', function() {
+      utils.isInteger(Infinity).should.equal(false);
+    });
+    it('will return false for Math.PI', function() {
+      utils.isInteger(Math.PI).should.equal(false);
+    });
+    it('will return false for "99"', function() {
+      utils.isInteger('99').should.equal(false);
+    });
+    it('will return false for "-99"', function() {
+      utils.isInteger('-99').should.equal(false);
+    });
+    it('will return true for 99', function() {
+      utils.isInteger(99).should.equal(true);
+    });
+    it('will return true for -99', function() {
+      utils.isInteger(-99).should.equal(true);
+    });
+  })
   describe('#setupDirectory', function() {
     it('will make directory if the application directory does not exist', function(done) {
       var mkdirp = sinon.stub().callsArg(1);
@@ -79,15 +109,94 @@ describe('Wallet Utils', function() {
     });
   });
   describe('#readJSONFile', function() {
-    it('', function() {
+    it('will give error from readFile', function(done) {
+      var enoentError = new Error();
+      enoentError.code = 'ENOENT';
+      var readFile = sinon.stub().callsArgWith(1, enoentError);
+      var utils = proxyquire('../lib/utils', {
+        'fs': {
+          readFile: readFile
+        }
+      });
+      utils.readJSONFile('/tmp/bwsv2-directory', function(err) {
+        should.exist(err);
+        err.code.should.equal('ENOENT')
+        readFile.callCount.should.equal(1);
+        done();
+      });
+    });
+    it('will give error from parsing JSON', function(done) {
+      var readFile = sinon.stub().callsArgWith(1, null, 'badjson');
+      var utils = proxyquire('../lib/utils', {
+        'fs': {
+          readFile: readFile
+        }
+      });
+      utils.readJSONFile('/tmp/bwsv2-directory', function(err) {
+        should.exist(err);
+        err.message.should.equal('Unexpected token b');
+        readFile.callCount.should.equal(1);
+        done();
+      });
+    });
+    it('will callback with parsed JSON', function(done) {
+      var readFile = sinon.stub().callsArgWith(1, null, '{"hello":"world"}');
+      var utils = proxyquire('../lib/utils', {
+        'fs': {
+          readFile: readFile
+        }
+      });
+      utils.readJSONFile('/tmp/bwsv2-directory', function(err, json) {
+        if (err) {
+          return done(err);
+        }
+        readFile.callCount.should.equal(1);
+        json.should.deep.equal({hello: 'world'});
+        done();
+      });
     });
   });
-  describe('#splitArray', function() {
-    it('', function() {
+  describe('#readWalletFile', function() {
+    var sandbox = sinon.sandbox.create();
+    afterEach(function() {
+      sandbox.restore();
+    });
+    it('it will call readWalletDatFile', function(done) {
+      sandbox.stub(utils, 'readJSONFile').callsArg(1);
+      utils.readWalletFile('wallet.json', 'testnet', function(err) {
+        if (err) {
+          done(err);
+        }
+        utils.readJSONFile.callCount.should.equal(1);
+        done();
+      });
+    });
+    it('it will call readJSONFile', function(done) {
+      sandbox.stub(utils, 'readWalletDatFile').callsArg(2);
+      utils.readWalletFile('wallet.dat', 'testnet', function(err) {
+        if (err) {
+          return done(err);
+        }
+        utils.readWalletDatFile.callCount.should.equal(1);
+        done();
+      });
+    });
+    it('will give error with unknown extension', function(done) {
+      utils.readWalletFile('wallet.unknown', 'testnet', function(err) {
+        should.exist(err);
+        err.message.should.equal('"dat" or "json" file extension is expected');
+        done();
+      });
     });
   });
   describe('#getRemoteAddress', function() {
-    it('', function() {
+    it('will return cloudflare headers', function() {
+      var address = utils.getRemoteAddress({headers: {'cf-connecting-ip': '127.0.0.1'}});
+      address.should.equal('127.0.0.1');
+    });
+    it('will return socket address', function() {
+      var address = utils.getRemoteAddress({headers: {}, socket: {remoteAddress: '127.0.0.1'}});
+      address.should.equal('127.0.0.1');
     });
   });
   describe('#enableCORS', function() {
@@ -103,12 +212,13 @@ describe('Wallet Utils', function() {
     });
   });
   describe('#getTaskId', function() {
-    it('', function() {
+    it('will get a random value', function() {
+      var id = utils.getTaskId();
+      id.should.be.a('string');
+      id.length.should.equal(8);
     });
   });
   describe('#getClients', function() {
-    it('', function() {
-    });
   });
   describe('#setClients', function() {
     it('', function() {
@@ -119,8 +229,94 @@ describe('Wallet Utils', function() {
     });
   });
   describe('#wrapRPCError', function() {
-    it('', function() {
+    it('will create an error', function() {
+      var error = utils.wrapRPCError({message: 'test'});
+      error.should.be.instanceOf(Error);
+      error.message.should.equal('test');
     });
   });
-
+  describe('#getAddressTypeString', function() {
+    it('it will return pubkeyhash with 01', function() {
+      utils.getAddressTypeString(new Buffer('01', 'hex')).should.equal('pubkeyhash');
+    });
+    it('it will return scripthash with 02', function() {
+      utils.getAddressTypeString(new Buffer('02', 'hex')).should.equal('scripthash');
+    });
+    it('will throw error if unknown type', function() {
+      (function() {
+        utils.getAddressTypeString(new Buffer('00', 'hex'));
+      }).should.throw('Unknown address type');
+    });
+  });
+  describe('#getAddressTypeBuffer', function() {
+    it('it will return 01 for pubkeyhash', function() {
+      utils.getAddressTypeBuffer({type: 'pubkeyhash'}).toString('hex').should.equal('01');
+    });
+    it('it will return 02 for scripthash', function() {
+      utils.getAddressTypeBuffer({type: 'scripthash'}).toString('hex').should.equal('02');
+    });
+    it('will throw error if unknown type', function() {
+      (function() {
+        utils.getAddressTypeBuffer({type: 'unkonwn'});
+      }).should.throw('Unknown address type');
+    });
+  });
+  describe('#splitBuffer', function() {
+    it('will split buffers by segment size', function() {
+      var value = 'f4d652a6902744f4738ab484c23d267293a2c7fdb89fe1aa78e6f0f2d0b63d2c';
+      value += '071fac61f3e6572197dcb9d7f5ddf17bcce062d12a29a1ba317c1c03d438ddef';
+      value += '97bdd4ac795eb08c6fd99b198bc9d8f549e3bc7e3fbb9e37563d8c9bf78bb477';
+      var buf = new Buffer(value, 'hex');
+      var buffers = utils.splitBuffer(buf, 32);
+      buffers.length.should.equal(3);
+      buffers[0].toString('hex').should.equal('f4d652a6902744f4738ab484c23d267293a2c7fdb89fe1aa78e6f0f2d0b63d2c');
+      buffers[1].toString('hex').should.equal('071fac61f3e6572197dcb9d7f5ddf17bcce062d12a29a1ba317c1c03d438ddef');
+      buffers[2].toString('hex').should.equal('97bdd4ac795eb08c6fd99b198bc9d8f549e3bc7e3fbb9e37563d8c9bf78bb477');
+    });
+  });
+  describe('#exitWorker', function() {
+    it('will send SIGINT and exit cleanly', function(done) {
+      var worker = new EventEmitter();
+      worker.kill = function() {
+        worker.emit('exit', 0);
+      };
+      utils.exitWorker(worker, 1000, function() {
+        done();
+      });
+    });
+    it('will send SIGINT and give error if non-zero', function(done) {
+      var worker = new EventEmitter();
+      worker.kill = function() {
+        worker.emit('exit', 1);
+      };
+      utils.exitWorker(worker, 1000, function(err) {
+        should.exist(err);
+        err.message.should.equal('Worker did not exit cleanly: 1');
+        done();
+      });
+    });
+    it('will send SIGKILL after timeout', function(done) {
+      var worker = new EventEmitter();
+      worker.kill = function(signal) {
+        if (signal === 'SIGKILL') {
+          worker.emit('exit', 1);
+        }
+      };
+      utils.exitWorker(worker, 1000, function(err) {
+        should.exist(err);
+        err.message.should.equal('Worker exit timeout, force shutdown');
+        done();
+      });
+    });
+  });
+  describe('#timestampToISOString', function() {
+    it('will convert second timestamp to iso string', function() {
+      utils.timestampToISOString(1231006505).should.equal('2009-01-03T18:15:05.000Z');
+    });
+  });
+  describe('#satoshisToBitcoin', function() {
+    it('1999 to 0.00001999', function() {
+      utils.satoshisToBitcoin(1999, 0.00001999);
+    });
+  });
 });
